b938fc319a78a27232bbe6a6d62ee99071b21b06
Vivek <vivek@pop-os.localdomain>
2023-06-10 19:18:06 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    
+    println!("Hello, world!");
+}
+
+fn train() {
+    let training_data : Vec<((u8, u8),  u8)> = vec![
+        ((1, 1), 0),
+        ((1, 0), 1),
+        ((0, 0), 0)
+    ];
+    
+
+    //single hidden layer. 
+
+
+
+}
+
