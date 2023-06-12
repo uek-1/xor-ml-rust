aed4d71d4c56bacea2c455e9f75eca258c4f8bfd
Vivek <vivek@pop-os.localdomain>
2023-06-12 09:49:20 +0530

    changed to f64

--- src/main.rs
+++ src/main.rs
@@ -3,49 +3,31 @@ use model::{Model, Layer};
 
 fn main() {
     let mut layers : Vec<Layer> = vec![];
-    let weights = vec![vec![1, 0], vec![0, 1]];
-    let biases = vec![0, 0];
+    let weights = vec![vec![1.0, 0.0], vec![0.0, 1.0]];
+    let biases = vec![0.0, 0.0];
     let hidden_layer_1 = Layer::new(weights,biases);
     layers.push(hidden_layer_1);
     
-    let weights = vec![vec![1,1]];
-    let biases = vec![0,0];
+    let weights = vec![vec![1.0,1.0]];
+    let biases = vec![0.0,0.0];
     let output_layer = Layer::new(weights,biases);
     layers.push(output_layer);
 
     let model = Model::from(layers);
  
-    let input = vec![1,1];
+    let input = vec![1.0,1.0];
     let res = model.evaluate(&input);
 
     println!("{res}");   
+    
+    let mut training_data : Vec<(Vec<f64>,  f64)> = vec![];
 
-    let model = train(model, 3);
+    for i in 0..5 {
+        training_data.push((vec![i as f64, i as f64], 15.0 * i as f64))
+    }
+
+    let model = model.to_trained(training_data, 3);
 
 }
   
-fn train(model: Model, epochs: usize) -> Model {
-    let training_data : Vec<(Vec<u8>,  u8)> = vec![
-        (vec![1, 1], 0),
-        (vec![1, 0], 1),
-        (vec![0, 0], 0)
-    ];
-
-    for i in 0..epochs {
-        println!("EPOCH {i}");
-        let mut total_cost : u8 = 0;
-
-        for (input, output) in &training_data {
-            total_cost += cost(&model, &input, *output);       
-        }
-        println!("TOTAL COST {total_cost}");
-    }
-
-    Model::new()
-    //single hidden layer. 
-}
-
-fn cost(model : &Model, input : &Vec<u8>, expected : u8) -> u8 {
-    (expected as i32 - model.evaluate(input) as i32).abs() as u8
- }
 
--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,18 @@
 #[derive(Clone)]
 pub struct Layer{
-    weights : Vec<Vec<u8>>,
-    biases : Vec<u8>,
+    weights : Vec<Vec<f64>>,
+    biases : Vec<f64>,
 }
 
 impl Layer {
-    pub fn new(weights : Vec<Vec<u8>>, biases : Vec<u8>) -> Layer {
+    pub fn new(weights : Vec<Vec<f64>>, biases : Vec<f64>) -> Layer {
         Layer {
             weights,
             biases
         }
     }
 
-    pub fn evaluate(&self, input: &Vec<u8>) -> Vec<u8> {
+    pub fn evaluate(&self, input: &Vec<f64>) -> Vec<f64> {
         matrix_column_multiply(&self.weights, &input).add_vec(&self.biases)
     }
 }
@@ -30,13 +30,36 @@ impl Model {
         Model(layers)
     }
 
-    pub fn evaluate(&self, input: &Vec<u8>) -> u8 {
+    pub fn evaluate(&self, input: &Vec<f64>) -> f64 {
         *self.0
             .iter()
             .fold(input.clone(), |temp, layer| layer.evaluate(&temp))
             .get(0)
             .unwrap()
     }
+
+    pub fn to_trained(&self, training_data : Vec<(Vec<f64>, f64)>, epochs: usize) -> Model {
+
+
+        for i in 0..epochs {
+            println!("EPOCH {i}");
+            let mut total_cost : f64 = 0.0;
+
+            for (input, output) in &training_data {
+                total_cost += self.cost(&input, *output);       
+            }
+            println!("TOTAL COST {total_cost}");
+        }
+
+        Model::new()
+        //single hidden layer. 
+    }
+
+    fn cost(&self, input : &Vec<f64>, expected : f64) -> f64 {
+        (expected - self.evaluate(input)).abs()
+    }
+
+
 }
 
 impl From<Vec<Layer>> for Model {
@@ -45,8 +68,8 @@ impl From<Vec<Layer>> for Model {
     }
 }
 
-impl AddVec<u8> for Vec<u8> {
-    fn add_vec(&self, other : &Vec<u8>) -> Vec<u8>{
+impl AddVec<f64> for Vec<f64> {
+    fn add_vec(&self, other : &Vec<f64>) -> Vec<f64>{
         self
             .iter()
             .zip(other.iter())
@@ -59,14 +82,14 @@ trait AddVec<T> {
     fn add_vec(&self, other : &Vec<T>) -> Vec<T>;
 }
 
-fn dot_product(left : &Vec<u8>, right : &Vec<u8>) -> u8 {
+fn dot_product(left : &Vec<f64>, right : &Vec<f64>) -> f64 {
     left
         .iter()
         .zip(right.iter())
-        .fold(0, |acc, (x,y)| acc + x * y)
+        .fold(0.0, |acc, (x,y)| acc + x * y)
 }
 
-fn matrix_column_multiply(mat : &Vec<Vec<u8>>, col : &Vec<u8>) -> Vec<u8> {
+fn matrix_column_multiply(mat : &Vec<Vec<f64>>, col : &Vec<f64>) -> Vec<f64> {
     mat
         .iter()
         .map(|row| dot_product(row, col))
@@ -79,18 +102,18 @@ mod test {
 
     #[test]
     fn dot_prod_test() {
-        let x : Vec<u8> = vec![1,2];
-        let y : Vec<u8> = vec![2,4];
+        let x : Vec<f64> = vec![1.0, 2.0];
+        let y : Vec<f64> = vec![2.0, 4.0];
         let res = dot_product(&x, &y);
 
-        assert_eq!(res, 10);
+        assert_eq!(res, 10.0);
     }
 
     #[test]
     fn matrix_column_multiply_test() {
-        let mat : Vec<Vec<u8>> = vec![vec![1, 0], vec![0, 1]];
-        let col : Vec<u8> = vec![2,3];
+        let mat : Vec<Vec<f64>> = vec![vec![1.0, 0.0], vec![0.0, 1.0]];
+        let col : Vec<f64> = vec![2.0, 3.0];
         let res = matrix_column_multiply(&mat, &col);
-        assert_eq!(res, vec![2,3]);
+        assert_eq!(res, vec![2.0,3.0]);
     }
 }
